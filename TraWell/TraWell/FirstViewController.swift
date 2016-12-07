9bfe59c752c945c3a54f52340b314e5ca31d15e5
Ajit Horo <ajithoro@gmail.com>
2016-12-08 01:22:18 +0530

    created project

---
+++ TraWell/TraWell/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  TraWell
+//
+//  Created by Nitin Kumar on 08/12/16.
+//  Copyright © 2016 Ajit Horo. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
