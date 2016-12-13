892d282a02d5b5da8c92f50a06d4a66c2f2ecfcc
Ajit Horo <ajithoro@gmail.com>
2016-12-13 20:00:50 +0530

    added tableview in first viewcontroller

---
+++ TraWell/TraWell/helpers/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  TraWell
+//
+//  Created by Unholy Saint on 13/12/16.
+//  Copyright © 2016 Ajit Horo. All rights reserved.
+//
+
+import Foundation
+
+let kCellTableView: String = "CellTableView"
--- TraWell/TraWell/viewcontrollers/FirstViewController.swift
+++ TraWell/TraWell/viewcontrollers/FirstViewController.swift
@@ -9,10 +9,14 @@
 import UIKit
 
 class FirstViewController: BaseViewController {
+    
+    @IBOutlet weak var tableViewPlaces: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.tableViewPlaces.delegate = self
+        self.tableViewPlaces.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +24,37 @@ class FirstViewController: BaseViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
 }
+
+extension FirstViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: kCellTableView, for: indexPath)
+        /*
+        cell.textLabel?.text = "abc"
+        cell.backgroundColor = UIColor.red
+        */
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 0 {
+            return "Just for the weekend"
+        }
+        return nil
+    }
+}
+
+extension FirstViewController: UITableViewDelegate {
+    
+}
+
 
